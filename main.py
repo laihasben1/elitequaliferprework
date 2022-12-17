fd3723c7b2468cee082db19989b923028ce3dd2b
Laila Hasben <92272233+laihasben1@users.noreply.github.com>
2022-12-17 19:08:18 +0000

    read me update

---
+++ main.py
@@ -0,0 +1,51 @@
+#convos and responses
+name_of_user = print("Chat Bot: Hello my name is Chat Bot, what's you're name?\n")
+
+user = input()
+
+print("Chat Bot: Nice to meet you %s\n" % user)
+
+what_color = print("Chat Bot: I would love to get to know you! What's your favorite color?\n")
+
+color = input() 
+
+nice_color = print("Chat Bot: Awesome! I also love %s although my favorite color is gold!\n" % color)
+
+what_fav_food = print("Chat Bot: What's your favorite type of food to eat?\n")
+
+food = input()
+
+fav_food = print("Chat Bot: Wow your fav food is %s ?! It's so funny because so is mine,!\n" %food)
+
+lockdown = print("Chat Bot: One quick question what was the hardest thing about being in the pandemic lockdown?\n")
+
+lockdown = input ()
+
+sad_lockdown = print("Chat Bot: :( aww that's sad, I'm happy that these past months have been getting things back to normal!\n")
+
+happy_netflix = print("Chat Bot: On a more happier note, what's your all time favorite movie?!\n")
+
+happy_netflix = input()
+
+response_netflix = print("Chat Bot: Cool your favorite movie is %s!, my all time favorite movie would have to be avengers end game for sure!\n" %happy_netflix)
+
+dream_place = print("Chat Bot: Let's do a question that you can use your imagination!, if you could go anywhere in the world where would you go?\n")
+
+dream_place = input()
+
+calculation_response = print("Chat Bot: Hmmm %s ? Let me look that in my data base and see what it looks like!\n" %dream_place)
+
+response_dream = print("Chat Bot: Based off of the images I've seen of %s looks like a beautiful place to go, I hope one day you will be able to make it there!\n" %dream_place)
+
+
+
+
+#end the convo
+reply = input ()
+
+if reply == "END" or reply == "quit" or reply == "exit" or reply == "end":
+  print("Nice talking to you have a nice day!")
+
+#back to regular
+
+
---
+++ readme.md
@@ -0,0 +1,3 @@
+Chat Bot!
+
+This project is clever and a little funny because you're talking to a robot!
